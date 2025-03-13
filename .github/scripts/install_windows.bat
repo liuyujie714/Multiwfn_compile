01f36b0b840cd56bce0994fdf88dd1d5032ea6af
liuyujie714 <1051690424@qq.com>
2025-03-13 21:36:29 +0800

    2022

--- .github/scripts/install_windows.bat
+++ .github/scripts/install_windows.bat
@@ -1,6 +1,4 @@
-REM SPDX-FileCopyrightText: 2022 Intel Corporation
-REM
-REM SPDX-License-Identifier: MIT
+REM Install Fortran compiler with Visual Studio 2022
 
 set URL=%1
 set COMPONENTS=%2
@@ -10,6 +8,14 @@ start /b /wait %TEMP%\webimage.exe -s -x -f webimage_extracted --log extract.log
 del %TEMP%\webimage.exe
 webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=.
 
+REM Configure environment variables
+@call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat" %VS_VER%
+
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+
+
 set installer_exit_code=%ERRORLEVEL%
 rd /s/q "webimage_extracted"
 exit /b %installer_exit_code%
+
