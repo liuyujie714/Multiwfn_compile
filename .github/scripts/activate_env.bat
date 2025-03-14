d34e569f1850cc39732e1e66a32ff88606cd5483
liuyujie714 <1051690424@qq.com>
2025-03-14 21:21:22 +0800

    test

--- .github/scripts/activate_env.bat
+++ .github/scripts/activate_env.bat
@@ -8,3 +8,4 @@ REM Copy libiomp5md.dll to working directory
 copy /Y "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\bin\libiomp5md.dll" .
 
 REM @call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2022
+
