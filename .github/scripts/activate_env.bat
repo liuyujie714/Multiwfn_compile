d58196e8b6bd65a0efe0da58d1bb9c71e1dba7e1
liuyujie714 <1051690424@qq.com>
2025-03-14 11:59:40 +0800

    Multiwfn 3.8 win use ninja

--- .github/scripts/activate_env.bat
+++ .github/scripts/activate_env.bat
@@ -6,3 +6,5 @@ for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\
 
 REM Copy libiomp5md.dll to working directory
 copy /Y "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\bin\libiomp5md.dll" .
+
+@call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2022
