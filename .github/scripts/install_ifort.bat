6b16bd9ddf9d15ecefd0bd682368731bd26ed9f0
liuyujie714 <1051690424@qq.com>
2025-03-15 10:05:28 +0800

    Update install_ifort.bat

--- .github/scripts/install_ifort.bat
+++ .github/scripts/install_ifort.bat
@@ -4,6 +4,7 @@ set URL=%1
 set COMPONENTS=%2
 
 curl.exe --output %TEMP%\webimage.exe --url %URL% --retry 5 --retry-delay 5
+%TEMP%\webimage.exe --list-components
 %TEMP%\webimage.exe -s -a --silent --eula accept --action install --components=%COMPONENTS% -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=1 --log-dir=.
 
 del %TEMP%\webimage.exe
